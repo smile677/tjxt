1b5044d9d3591ec0cecb66836d9c708ddcd51bbb
smile67 <1639316332@qq.com>
2024-08-19 15:37:22 +0800

    feat: 使用XXL-JOB定时创建榜单表

--- tj-learning/src/main/java/com/tianji/learning/task/PointsBoardPersistentHandler.java
+++ tj-learning/src/main/java/com/tianji/learning/task/PointsBoardPersistentHandler.java
@@ -2,9 +2,9 @@ package com.tianji.learning.task;
 
 import com.tianji.learning.domain.po.PointsBoardSeason;
 import com.tianji.learning.service.IPointsBoardSeasonService;
+import com.xxl.job.core.handler.annotation.XxlJob;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
@@ -20,9 +20,10 @@ public class PointsBoardPersistentHandler {
      * 创建上赛季（上个月）榜单表
      */
     // 每月1号凌晨三点运行
-    @Scheduled(cron = "0 0 3 1 * ?")
+//    @Scheduled(cron = "0 0 3 1 * ?")
     // 测试用 下一分钟
 //    @Scheduled(cron = "0 1 2 19 8 ?")
+    @XxlJob("createTableJob")
     public void createPointsBoardTableOfLastSeason() {
         log.info("创建上赛季积分榜");
         // 1.获取上个月当前时间点
