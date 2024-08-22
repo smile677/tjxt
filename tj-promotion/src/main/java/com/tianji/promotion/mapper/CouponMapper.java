ba9e483f1ee7bbd67ad0927210c5ead817fa3fcb
smile67 <1639316332@qq.com>
2024-08-22 23:53:00 +0800

    pref: 乐观锁优化

--- tj-promotion/src/main/java/com/tianji/promotion/mapper/CouponMapper.java
+++ tj-promotion/src/main/java/com/tianji/promotion/mapper/CouponMapper.java
@@ -14,7 +14,7 @@ import org.apache.ibatis.annotations.Update;
 public interface CouponMapper extends BaseMapper<Coupon> {
     // 更新优惠券已领取梳理
     @Update("update coupon set issue_num = issue_num + 1 where id = #{id} and issue_num < total_num")
-    void incrIssueNum(@Param("id") Long id);
+    int incrIssueNum(@Param("id") Long id);
 }
 
 
