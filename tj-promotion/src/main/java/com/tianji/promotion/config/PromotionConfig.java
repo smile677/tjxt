ad3316bcb1b9af96c9cc9cb0ef75ae1d4cfaa1e5
smile67 <1639316332@qq.com>
2024-08-22 11:37:06 +0800

    feat: 登录拦截放行

--- tj-promotion/src/main/java/com/tianji/promotion/config/PromotionConfig.java
+++ tj-promotion/src/main/java/com/tianji/promotion/config/PromotionConfig.java
@@ -7,6 +7,9 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.ThreadPoolExecutor;
 
+/**
+ * 自定义线程池
+ */
 @Slf4j
 @Configuration
 public class PromotionConfig {
